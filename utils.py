89e023dccc13075b5ec161ff80e9b75e66d4c79c
damlacoskun <58702222+damlacoskun@users.noreply.github.com>
2025-07-10 22:02:14 +0300

    Add files via upload
    SE att eklendi

--- utils.py
+++ utils.py
@@ -1107,3 +1107,26 @@ class dataloaders_recon():
                                                   shuffle=False, num_workers=4)
 
         return train_loader, test_loader1, test_loader2
+
+import torch.nn as nn
+import torch
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        # Squeeze
+        y = self.avg_pool(x).view(b, c)
+        # Excitation
+        y = self.fc(y).view(b, c, 1, 1)
+        # Scale
+        return x * y.expand_as(x)
